3a4168c273cf4086678813d15f2aafcf82e1511d
maarten peels <maartenpeels@gmail.com>
2016-04-06 13:38:00 +0200

    Changed Types

--- src/com/busenzo/administratie/Administratie.java
+++ src/com/busenzo/administratie/Administratie.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import com.busenzo.domein.Bus;
 import com.busenzo.domein.Lijn;
 import com.busenzo.domein.Halte;
+import com.busenzo.domein.Richting;
 import java.util.Collections;
 import java.util.List;
 import java.io.BufferedReader;
@@ -27,7 +28,8 @@ public class Administratie {
      * URL opgebouwd via Restserver/restkey/query
      * Geeft JSON object terug
      */
-    public JSONObject getJSONfromWeb(String query) throws Exception{
+    public JSONObject getJSONfromWeb(String query) throws Exception
+    {
         String getUrl = this.restServer + "/" + this.restKey + "/" + query;
         JSONParser parser = new JSONParser();
         String json = readUrl(getUrl);
@@ -40,29 +42,49 @@ public class Administratie {
      * URL datareader functie
      * Geeft databuffer terug
      */
-    private static String readUrl(String urlString) throws Exception {
+    private static String readUrl(String urlString) throws Exception 
+    {
         BufferedReader reader = null;
-        try {
+        try 
+        {
             URL url = new URL(urlString);
             reader = new BufferedReader(new InputStreamReader(url.openStream()));
             StringBuilder buffer = new StringBuilder();
             int read;
             char[] chars = new char[1024];
             while ((read = reader.read(chars)) != -1)
+            {
                 buffer.append(chars, 0, read); 
+            }
 
             return buffer.toString();
-        } finally {
+        } finally 
+        {
             if (reader != null)
+            {
                 reader.close();
+            }
         }
 
+    }
+    
+    private Halte findStop(String id)
+    {
+        for (Iterator<Halte> it = haltes.iterator(); it.hasNext();) {
+            Halte h = it.next();
+            if(h.getId() == id)
+            {
+                return h;
+            }
+        }
+        return null;
     }
 
     /**
      * Maak een nieuwe administratie aan
      */
-    public Administratie() {
+    public Administratie() 
+    {
             //throw new UnsupportedOperationException();
     }
 
@@ -71,14 +93,16 @@ public class Administratie {
      * @param id: ID (chassisnummer) van de bus
      * @return De bus met het ingevoerde id, null als deze niet bestaat
      */
-    public Bus zoekBus(int id) {
+    public Bus zoekBus(int id) 
+    {
             throw new UnsupportedOperationException();
     }
 
     /**
      * Update de locaties van alle bussen (deze worden opgehaald uit de database)
      */
-    public void HaalBusLocaties() {
+    public void HaalBusLocaties() 
+    {
             throw new UnsupportedOperationException();
     }
 
@@ -88,7 +112,8 @@ public class Administratie {
      * @return Een lijst van alle lijnen die bij deze halte stoppen, kan een lege lijst zijn als de halte 
      * niet gevonden wordt
      */
-    public List<Lijn> GeefHalteInformatie(String halteNaam) {
+    public List<Lijn> GeefHalteInformatie(String halteNaam) 
+    {
             throw new UnsupportedOperationException();
     }
 
@@ -97,7 +122,8 @@ public class Administratie {
      * @param nummer: Het lijnnummer waarvan je de haltes wil weten.
      * @return Een lijst van haltes waar deze lijn stopt, kan leeg zijn als het lijnnummer niet gevonden wordt
      */
-    public List<Halte> GeefLijnInformatie(String nummer) {
+    public List<Halte> GeefLijnInformatie(String nummer) 
+    {
             throw new UnsupportedOperationException();
     }
 
@@ -105,7 +131,8 @@ public class Administratie {
      * Voeg een nieuwe bus toe aan de administratie
      * @param bus: De toe te voegen bus, er mag nog geen bus bestaan met hetzelfde chassisnummer 
      */
-    public void addBus(Bus bus){
+    public void addBus(Bus bus)
+    {
         throw new UnsupportedOperationException();
     }
 
@@ -114,7 +141,8 @@ public class Administratie {
      * @param bussen: Een lijst met bussen die toegevoegd worden. Bussen worden alleen toegevoegd als er nog
      * geen bus bestaat met dat specifieke chassisnummer
      */
-    public void addBussen(List<Bus> bussen){
+    public void addBussen(List<Bus> bussen)
+    {
         throw new UnsupportedOperationException();
     }
 
@@ -122,7 +150,8 @@ public class Administratie {
      * Verwijder een specifieke bus uit de administratie
      * @param bus De te verwijderen bus
      */
-    public void removeBus(Bus bus){
+    public void removeBus(Bus bus)
+    {
         throw new UnsupportedOperationException();
     }
 
@@ -130,7 +159,8 @@ public class Administratie {
      * Voeg een halte toe aan de administratie
      * @param halte De toe te voegen halte, deze wordt alleen toegevoegd als deze halte nog niet bestaat
      */
-    public void addHalte(Halte halte){
+    public void addHalte(Halte halte)
+    {
         throw new UnsupportedOperationException();
     }
 
@@ -139,7 +169,8 @@ public class Administratie {
      * @param haltes Een lijst van toe te voegen haltes, haltes worden alleen toegevoegd als deze nog niet 
      * bestaan
      */
-    public void addHaltes(List<Halte> haltes){
+    public void addHaltes(List<Halte> haltes)
+    {
         throw new UnsupportedOperationException();
     }
 
@@ -147,7 +178,8 @@ public class Administratie {
      * Voeg een specifieke lijn toe aan de administratie
      * @param lijn De toe te voegen lijn. Deze wordt alleen toegevoegd als deze lijn nog niet bestaat.
      */
-    public void addLijn(Lijn lijn){
+    public void addLijn(Lijn lijn)
+    {
         throw new UnsupportedOperationException();
     }
 
@@ -155,7 +187,8 @@ public class Administratie {
      * Voeg meerdere lijnene toe aan de administratie
      * @param lijnen De toe te voegen lijnen. Lijnen worden alleen toegevoegd als ze nog niet bestaan
      */
-    public void addLijnen(List<Lijn> lijnen){
+    public void addLijnen(List<Lijn> lijnen)
+    {
         throw new UnsupportedOperationException();
     }
 
@@ -163,7 +196,8 @@ public class Administratie {
      * haal de lijst van haltes op
      * @return een onwijzigbare lijst van haltes
      */
-    public ArrayList<Halte> getHaltes() {
+    public ArrayList<Halte> getHaltes() 
+    {
         return this.haltes;
     }
     public void getHalteData() throws Exception
@@ -173,13 +207,13 @@ public class Administratie {
         JSONArray halteArray = (JSONArray) halteData.get("data");
         for(int i = 0; i < halteArray.size(); i++)
         {
-              JSONObject objects = (JSONObject)halteArray.get(i);
-              String halteID = objects.get("id").toString();
-              String halteNaam = objects.get("name").toString();
-              String halteLat = objects.get("lat").toString();
-              String halteLon = objects.get("lon").toString();
-              Halte addHalte = new Halte(halteID, halteNaam, halteLon, halteLat);
-              this.haltes.add(addHalte);
+            JSONObject objects = (JSONObject)halteArray.get(i);
+            String halteID = objects.get("id").toString();
+            String halteNaam = objects.get("name").toString();
+            String halteLat = objects.get("lat").toString();
+            String halteLon = objects.get("lon").toString();
+            Halte addHalte = new Halte(halteID, halteNaam, halteLon, halteLat);
+            this.haltes.add(addHalte);
         }
         System.out.println("Added " + this.haltes.size() + " to application");
     }
@@ -190,13 +224,43 @@ public class Administratie {
         JSONArray rittenArray = (JSONArray) rittenData.get("data");
         for(int i = 0; i < rittenArray.size(); i++)
         {
-              JSONObject objects = (JSONObject)rittenArray.get(i);
-              String ritID = objects.get("id").toString();
-              String halteNaam = objects.get("name").toString();
-              String halteLat = objects.get("lat").toString(); //Nog niet af :(
-              String halteLon = objects.get("lon").toString();
-              Halte addHalte = new Halte(ritID, halteNaam, halteLon, halteLat);
-              this.haltes.add(addHalte);
+            JSONObject objects = (JSONObject)rittenArray.get(i);
+            String ritID = objects.get("id").toString();
+            String halteNaam = objects.get("name").toString();
+            String halteLat = objects.get("lat").toString(); //Nog niet af :(
+            String halteLon = objects.get("lon").toString();
+            Halte addHalte = new Halte(ritID, halteNaam, halteLon, halteLat);
+            this.haltes.add(addHalte);
+        }
+        System.out.println("Added " + this.haltes.size() + " to application");
+    }
+    public void getLineData() throws Exception
+    {
+        String query = "lijnen";
+        JSONObject lijnenData = this.getJSONfromWeb(query);
+        JSONArray lijnenArray = (JSONArray) lijnenData.get("data");
+        for(int i = 0; i < lijnenArray.size(); i++)
+        {
+                JSONObject objects = (JSONObject)lijnenArray.get(i);
+                String lijnId = objects.get("id").toString();
+                int lijnNummer = Integer.parseInt(objects.get("linenum").toString());
+                Richting direction = objects.get("direction").toString() == "0" ? Richting.HEEN : Richting.TERUG;
+                String beschrijving = objects.get("name").toString();//direction, halte->id
+              
+                ArrayList<Halte> tempHaltes = new ArrayList<>();
+                JSONArray haltesArray = (JSONArray) objects.get("stops");
+                for(i = 0; i < haltesArray.size(); i++)
+                {
+                    objects = (JSONObject)haltesArray.get(i);
+                    Halte h = findStop(objects.get("id").toString());
+                    if(h != null)
+                    {
+                        tempHaltes.add(h);
+                    }
+                }
+              
+                Lijn addLijn = new Lijn(lijnId, lijnNummer, direction, beschrijving, tempHaltes);
+                this.lijnen.add(addLijn);
         }
         System.out.println("Added " + this.haltes.size() + " to application");
     }
