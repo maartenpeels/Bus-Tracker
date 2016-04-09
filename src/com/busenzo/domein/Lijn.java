2044bfb825b39f69a0be09929ff0c5651cc635f6
sguldemond <s.guldemond@student.fontys.nl>
2016-04-09 19:47:44 +0200

    addHalte, addHaltes & addRit return values aangepast naar boolean bovenstaande methode inhoudelijk aangepast per return value

--- src/com/busenzo/domein/Lijn.java
+++ src/com/busenzo/domein/Lijn.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Lijn {
-    
+
     private final String id;
     private final int nummer;
     private final Richting richting;
@@ -13,67 +13,106 @@ public class Lijn {
     public ArrayList<Halte> haltes = new ArrayList<>();
 
     /**
-     * Maak een nieuwe lijn aan. Er mag nog geen lijn bestaan met hetzelfde nummer, richting en beschrijving.
+     * Maak een nieuwe lijn aan. Er mag nog geen lijn bestaan met hetzelfde
+     * nummer, richting en beschrijving.
+     *
      * @param id: LijnID geleverd door ov database
      * @param nummer: Het lijnnummer
      * @param richting: De richting van de lijn
      * @param beschrijving Beschrijving van de lijn.
      */
     public Lijn(String id, int nummer, Richting richting, String beschrijving) {
-            this.id = id;
-            this.nummer = nummer;
-            this.richting = richting;
-            this.beschrijving = beschrijving;
-            this.haltes = haltes;
+        this.id = id;
+        this.nummer = nummer;
+        this.richting = richting;
+        this.beschrijving = beschrijving;
+        this.haltes = haltes;
     }
 
     /**
      * Voeg een halte toe aan deze lijn
-     * @param halte: De toe te voegen halte, deze halte mag bij deze lijn nog niet bestaan.
+     *
+     * @param halte: De toe te voegen halte, deze halte mag bij deze lijn nog
+     * niet bestaan.
      */
-    public void addHalte(Halte halte) {
-            this.haltes.add(halte);
+    public boolean addHalte(Halte halte) {
+        for (Halte h : haltes) {
+            if (h.getId() == halte.getId()) {
+                return false;
+            }
+        }
+
+        this.haltes.add(halte);
+        return true;
     }
 
     /**
      * Voeg een lijst van haltes toe aan deze lijn
-     * @param haltes: Een lijst van de toe te voegen haltes, iedere individuele halte mag bij deze lijn
-     * nog niet bestaan
+     *
+     * @param haltes: Een lijst van de toe te voegen haltes, iedere individuele
+     * halte mag bij deze lijn nog niet bestaan
      */
-    public void addHaltes(Halte[] haltes) {
-            for(Halte h : haltes) this.haltes.add(h);
+    public boolean addHaltes(Halte[] haltes) {
+        for (Halte h1 : this.haltes) {
+            for (Halte h2 : haltes) {
+                if (h1.getId() == h2.getId()) {
+                    return false;
+                }
+
+            }
+        }
+
+        for (Halte h3 : haltes) {
+            this.haltes.add(h3);
+        }
+        return true;
+        
+
     }
 
     /**
      * Haal de ritten op die bij deze lijn horen.
+     *
      * @return Een lijst van ritten die bij deze lijn horen
      */
-    public List<Rit> haalRitten() {
-            return this.ritten;
+    public List<Rit> getRitten() {
+        return this.ritten;
     }
 
     /**
      * Voeg een rit toe aan de lijn.
-     * @param rit De toe te voegen rit. Deze rit mag bij deze lijn nog niet bestaan.
+     *
+     * @param rit De toe te voegen rit. Deze rit mag bij deze lijn nog niet
+     * bestaan.
      */
-    public void addRit(Rit rit) {
-            this.ritten.add(rit);
+    public boolean addRit(Rit rit) {
+        for(Rit r : ritten)
+        {
+            if(r.getLijn().getId() == rit.getLijn().getId() && r.getVerwachteAankomstTijd() == rit.getVerwachteAankomstTijd()) {
+                return false;
+            }
+        }
+        
+        this.ritten.add(rit);
+        return true;
     }
 
     /**
      * Vraag het lijnnummer op
+     *
      * @return Het lijnnummer van deze lijn
      */
     public int getNummer() {
         return nummer;
     }
-    
+
     public String getId() {
         return this.id;
     }
 
     /**
      * Vraag de richting op van de lijn (heen of terug)
+     *
      * @return De richting van de lijn
      */
     public Richting getRichting() {
@@ -82,6 +121,7 @@ public class Lijn {
 
     /**
      * Vraag de beschrijvin van de lijn op
+     *
      * @return De lijnbeschrijving
      */
     public String getBeschrijving() {
